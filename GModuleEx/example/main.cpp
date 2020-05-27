42dba3256d2430eb8bf3b83debd2b069c877ea01
Rajesh Kumar Sahoo <sahoorajesh.d@gmail.com>
2020-05-27 06:33:42 +0000

    acceptiong module name from cmd line

--- GModuleEx/example/main.cpp
+++ GModuleEx/example/main.cpp
@@ -8,7 +8,7 @@ int main (int argc, char *argv[]) {
     gpointer func;
     init_fun_t init_fun;
     int retVal;
-
+    const char *module_name = (argc==2) ? argv[1] : "../src/libstudygmodule.dll";
     if (!g_module_supported())
     {
         LogDebug() << "Dynamic Opening of modules is not supported\n";
@@ -18,7 +18,7 @@ int main (int argc, char *argv[]) {
     /* G_MODULE_BIND_LAZY is overridden and the module is opened with
      * flag G_MODULE_BIND_LOCAL
      */
-    module = g_module_open("../src/libstudygmodule.dll", G_MODULE_BIND_LAZY);
+    module = g_module_open(module_name, G_MODULE_BIND_LAZY);
 
     // 1 is the ordinal number for gplugin_a_func1
     if(module && g_module_symbol(module, "plugin_init" ,&func))
